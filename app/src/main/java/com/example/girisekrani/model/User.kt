5b47aad2fa12eef3eb60d843fad8927f80e1db86
enesyardibi <enesyardibi@gmail.com>
2025-10-16 17:45:55 +0300

    Cleanup: remove deprecated bridge files after MVVM refactor

--- app/src/main/java/com/example/girisekrani/model/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.girisekrani.model
-
-@Deprecated("Moved to com.example.girisekrani.domain.model.User", ReplaceWith("com.example.girisekrani.domain.model.User"))
-typealias User = com.example.girisekrani.domain.model.User
-
--- app/src/main/java/com/example/girisekrani/repository/AuthRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.girisekrani.repository
-
-@Deprecated("Moved to com.example.girisekrani.data.repository.AuthRepository", ReplaceWith("com.example.girisekrani.data.repository.AuthRepository"))
-typealias AuthRepository = com.example.girisekrani.data.repository.AuthRepository
-
